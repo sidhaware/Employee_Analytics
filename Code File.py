3f0317c5ecb3d805dde9fda0207e11e99239aaf5
Siddhesh Haware <82457982+sidhaware@users.noreply.github.com>
2023-09-29 18:45:50 +0530

    Add files via upload

---
+++ Code File.py
@@ -0,0 +1,34 @@
+import joblib
+import pickle
+import streamlit as st
+import sklearn
+
+# model = joblib.load(r'C:\Users\Dell1\Desktop\decision_tree_model.pkl')
+model = pickle.load(open(r'C:\Users\Dell1\Desktop\DeploymentProject\Streamlit\rf_model.pkl','rb'))
+
+def main():
+    st.title('Employee Churn Predictor')
+
+    #input Variables
+    # 'children', 'Claim_Amount', 'past_consultations','Hospital_expenditure', 'Anual_Salary'
+    lv=st.text_input('Last evaluation score (normalized value between 0 to 1)')
+    np =st.text_input('Number of projects completed')
+    ty =st.text_input('Total years completed in the Company')
+    sl=st.text_input('Salary (1 - Low category \n 2 - Medium category \n 3 - High category)')
+   
+
+
+    #prediction code
+    if st.button('Predict'):
+        makeprediction = model.predict([[float(lv), float(np), float(ty), float(sl)]])
+        output=round (makeprediction[0], 2)
+        if output==1:
+            st.success('The employee is likely to churn')
+        else:
+            st.success('The employee is not likely to churn')
+
+        
+
+if __name__ == '__main__':
+    main()
+
